ce66da6eaa6f03e3851c8b9f07e654139e58e87b
Ajsi Sala <AjsSal20@htl-shkoder.com>
2025-03-04 12:48:27 +0100

    erster commit api

--- src/routes/api/hotels/[uuid]/+server.js
+++ src/routes/api/hotels/[uuid]/+server.js
@@ -1,5 +1,33 @@
 
 import { createConnection } from '$lib/db/mysql';
+import {BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWORD} from '$env/static/private';
+async function authenticate(request) {
+ 
+    const authHeader = request.headers.get('authorization');
+   
+    if (!authHeader){
+        return new Response(null,{
+            status: 401,
+            headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"'}
+        });
+   
+    }
+   
+   
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = atob(base64Credentials);
+    const [username, password] = credentials.split(':');
+   
+   
+    if (username !== BASIC_AUTH_USERNAME || password !== BASIC_AUTH_PASSWORD){
+        return new Response (JSON.stringify({message:'Access denied'}), {
+            status: 401,
+            headers: {'Content-Type': 'application/json'},
+        });
+    }
+    return null;
+   
+    }
 
 
 export async function GET({ params }) {
@@ -15,6 +43,8 @@ export async function GET({ params }) {
 
 
   export async function DELETE({ params }) {
+    const auth = await authenticate(request);
+    if (auth) return auth;
     const { uuid } = params;
     const connection = await createConnection();
  
