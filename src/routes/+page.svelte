f73af7e6679005cc6b6d861365e7b5b02a26638d
Ajsi Sala <AjsSal20@htl-shkoder.com>
2025-03-04 12:51:33 +0100

    erster commit api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Hotels API</h1>
+<p>Visit <a href="/api/hotels">this site</a> to read the documentation</p>
